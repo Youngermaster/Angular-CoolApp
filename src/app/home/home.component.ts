90a7d71e8bbd87053799c9ae05287451e918a7ca
Youngermaster <jmyoungh@eafit.edu.co>
2019-09-07 03:17:33 -0500

    I finished my first Angular Web App

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
+  name: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter++;
+  }
+
+  setStyle() {
+    let myStyle = {
+      active: this.clickCounter > 4,
+      noActive: this.clickCounter <= 4
+    }
+    return myStyle;
+  }
+
+}
